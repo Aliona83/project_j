d542b03eb5e2ed28c5e9dc23621704228f3a6005
Aliona83 <alionajanusko@gmail.com>
2023-07-14 12:52:31 +0000

    Create models for category and jewellery

--- jewelleries/admin.py
+++ jewelleries/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Jewellery, Category
 
 # Register your models here.
+admin.site.register(Jewellery)
+admin.site.register(Category)
--- jewelleries/models.py
+++ jewelleries/models.py
@@ -10,3 +10,17 @@ class Category(models.Model):
 
     def get_friendly_name(self):
         return self.friendly_name
+
+
+class Jewellery(models.Model):
+    name = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_featured = models.BooleanField(default=False, verbose_name="Feature on Home Page")
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
