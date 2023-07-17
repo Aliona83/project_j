e96cc191d32c047fb286a2b9a775fc5e3d4c1822
Aliona83 <alionajanusko@gmail.com>
2023-07-17 10:28:38 +0000

    Customized admin

--- jewelleries/admin.py
+++ jewelleries/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import Jewellery, Category
 
 # Register your models here.
+
+class JewelleryAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 admin.site.register(Jewellery)
 admin.site.register(Category)
