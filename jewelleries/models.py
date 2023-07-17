eaa819c8b537ac8f5fde8e065f9a0ef5de05d785
Aliona83 <alionajanusko@gmail.com>
2023-07-17 10:16:35 +0000

    Added jewwllery app, models and fixtures

--- jewelleries/models.py
+++ jewelleries/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
-
+    
+    
     def __str__(self):
         return self.name
 
@@ -13,7 +18,7 @@ class Category(models.Model):
 
 
 class Jewellery(models.Model):
-    name = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
