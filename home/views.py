875bd4ee41d0ad47f78868453a4aac8ef93c403c
Aliona83 <alionajanusko@gmail.com>
2023-07-19 16:31:22 +0000

    Added owl carousel files in project static directory,create folder for owl carousel

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from jewelleries.models import Jewellery
 
+
 def index(request):
     """ A view to return the index page """
     featured_jewellery = Jewellery.objects.filter(is_featured=True)
