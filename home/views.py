fbbf85e92d2b0b692ae66c96b4f12a8a091886e2
Aliona83 <alionajanusko@gmail.com>
2023-07-19 08:41:00 +0000

    added structure of carousel for my features in home page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from jewelleries.models import Jewellery
 
 def index(request):
     """ A view to return the index page """
+    featured_jewellery = Jewellery.objects.filter(is_featured=True)
 
-    return render(request, 'home/index.html')
+    context = {
+        'featured_jewellery': featured_jewellery,
+}
+    return render(request, 'home/index.html', context)
