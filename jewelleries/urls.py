eade3167b2783716214890097d03e0998d1ec8d5
Aliona83 <alionajanusko@gmail.com>
2023-07-20 15:11:50 +0000

    Remove owl carousel as was blocking navigation bar functionality

--- jewelleries/urls.py
+++ jewelleries/urls.py
@@ -5,6 +5,6 @@ app_name = 'jewelleries'  # This sets the app namespace to 'jewelleries'
 
 
 urlpatterns = [
-    path('', views.all_jewelleries, name='all_jewelleries'),
+    path('', views.all_jewelleries, name='jewelleries'),
     path('jewelleries/<int:jewellery_id>', views.jewelleries_details, name='jewelleries_details')
 ]
