27c483aa0776a9957e2b82b955fb2f479740db4f
Aliona83 <alionajanusko@gmail.com>
2023-07-21 14:20:10 +0000

    Added ssort by functionality

--- jewelleries/views.py
+++ jewelleries/views.py
@@ -22,6 +22,8 @@ def all_jewelleries(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 jewelleries= jewelleries.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
