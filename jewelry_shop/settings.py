2174ca557507d0dbf053e9fcfd463300d045b8bf
Aliona83 <alionajanusko@gmail.com>
2023-07-27 09:10:14 +0000

    Added checkout view and templates

--- jewelry_shop/settings.py
+++ jewelry_shop/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
     'jewelleries',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +62,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'jewelry_shop.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -74,7 +79,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
