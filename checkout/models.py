47c1dbddec1a3c4789d63fde6e7783f62fb4d524
Aliona83 <alionajanusko@gmail.com>
2023-07-27 06:58:24 +0000

    Create checkout app and models

--- checkout/models.py
+++ checkout/models.py
@@ -1,4 +1,11 @@
+import uuid
+
 from django.db import models
+from django.db.models import Sum
+from django.conf import settings
+
+from jewelleries.models import Jewellery
+
 
 # Create your models here.
 class Order(models.Model):
@@ -13,6 +20,66 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    discount = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    discount_apply = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
     order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+
+
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID
+        """
+        return uuid.uuid4().hex.upper()
+
+    def update_total(self):
+        """
+        Update grand total each time a line item is added,
+        accounting for delivery costs.
+        """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total > settings.DISCOUNT_PER_1000_EURO:
+            self.discount_apply = self.discount = (self.order_total // 1000 ) * settings.DISCOUNT_PER_1000_EURO
+        else:
+            self.discount_apply = self.discount = 0
+        self.grand_total = self.order_total - self.discount_apply
+        self.save()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+
+    def __str__(self):
+        return self.order_number
+
+
+class OrderLineItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Jewellery, null=False, blank=False, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=0)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the lineitem total
+        and update the order total.
+        """
+        self.lineitem_total = self.product.price * self.quantity
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
+
+
+    def create_order_from_bag(bag_items):
+        order = Order.objects.create()
+        for item in bag_items:
+            product = item['product']
+            quantity = item['quantity']
+            OrderLineItem.objects.create(order=order, product=product, quantity=quantity)
+        order.update_total()
+        return order
--- jewelry_shop/settings.py
+++ jewelry_shop/settings.py
@@ -157,7 +157,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-DISCOUNT_PER_1000_EURO= 100
+DISCOUNT_PER_1000_EURO = 100
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
