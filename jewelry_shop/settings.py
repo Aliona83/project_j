37ece767de64534a77d6a3dc56163db446c53796
Aliona83 <alionajanusko@gmail.com>
2023-08-24 13:58:48 +0000

    Fixed indentation on line 200

--- jewelry_shop/settings.py
+++ jewelry_shop/settings.py
@@ -197,7 +197,7 @@ if 'USE_AWS' in os.environ:
 
     # Override static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
-     MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Stripe
 DISCOUNT_PER_1000_EURO = 100
