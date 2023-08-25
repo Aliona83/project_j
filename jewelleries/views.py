bd8a9b3e5e8d69cf5cb9f5773bb6bd6364d8d2b6
Aliona83 <alionajanusko@gmail.com>
2023-08-25 14:20:18 +0000

    Fixed indentation on jewellery app view.py

--- jewelleries/views.py
+++ jewelleries/views.py
@@ -1,89 +1,89 @@
-from django.shortcuts import render, reverse, redirect
-from .models import Jewellery, Category
-from django.shortcuts import get_object_or_404
-from django.db.models import Q
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.db.models.functions import Lower
-from .forms import ProductForm
 
-# Import Pagination Stuff
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Jewellery, Category
 
-# @login_required
-
+# Create your views here.
 
 def all_jewelleries(request):
-    """  A view to show all jewelleries, including sorting and search queries """
+    """ A view to show all products, including sorting and search queries """
+
     jewelleries = Jewellery.objects.all()
     query = None
     categories = None
     sort = None
     direction = None
 
-# Set up Pagination
-    p = Paginator(jewelleries, 16)
-    page = request.GET.get('page', 1)
-
-    try:
-        all_jewellery = p.page(page)
-    except (EmptyPage, PageNotAnInteger):
-        all_jewellery = p.page(1)
-        
     if request.GET:
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
             if sortkey == 'name':
                 sortkey = 'lower_name'
-                jewelleries = jewelleries.annotate(lower_name=Lower('name'))
+                jewelleries= jewelleries.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             jewelleries = jewelleries.order_by(sortkey)
-    if request.GET:
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             jewelleries = jewelleries.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
-    if request.GET:
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, 'You didnt enter search criteria !')
-                return redirect(reverse('jewelleries:jewelleries'))
-            queries = Q(name__icontains=query)| Q(description__icontains=query)
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
             jewelleries = jewelleries.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
+
     context = {
         'jewelleries': jewelleries,
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
-        'all_jewellery': all_jewellery,
     }
+
     return render(request, 'jewelleries/jewelleries.html', context)
 
 
-def jewelleries_details(request, jewellery_id):
-    """  A view to show individual product details """
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    jewelleries = get_object_or_404(Jewellery, pk=jewellery_id)
+
+    context = {
+        'jewellery': jewellery,
+    }
+
+    return render(request, 'jewelleries/jewelleries.html', context)
+
+def jewelleries_details(request, product_id):
+    """ A view to show individual product details """
+
     jewellery = get_object_or_404(Jewellery, pk=jewellery_id)
 
     context = {
         'jewellery': jewellery,
     }
-    return render(request, 'jewelleries/jewelleries_details.html', context)
 
+    return render(request, 'jewelleries/jewelleries_detail.html', context)
+   
 
 @login_required
 def add_jewellery(request):
-    """ Add a product to the store """
+    """Add a product to the store"""
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
@@ -104,10 +104,9 @@ def add_jewellery(request):
     }
     return render(request, template, context)
 
-
 @login_required
 def edit_jewellery(request, jewellery_id):
-    """ Edit a product in the store """
+    """Edit a product in the store"""
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
@@ -130,18 +129,16 @@ def edit_jewellery(request, jewellery_id):
         'form': form,
         'jewellery': jewellery,
     }
-
     return render(request, template, context)
-
 
 @login_required
 def delete_jewellery(request, jewellery_id):
-    """ Delete a product from the store """
+    """Delete a product from the store"""
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, only store owners can do that.')
         return redirect(reverse('home'))
 
     jewellery = get_object_or_404(Jewellery, pk=jewellery_id)
     jewellery.delete()
-    messages.success(request, 'Jewellery was successful  deleted!')
+    messages.success(request, 'Jewellery was successfully deleted!')
     return redirect(reverse('jewelleries:jewelleries'))
