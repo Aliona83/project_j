600ce4870eaa1c19f9cf10d629a9614efdce75b7
Aliona83 <alionajanusko@gmail.com>
2023-08-28 12:06:13 +0000

    Create templates submitted_success and submitted_form

--- CustomOrder/views.py
+++ CustomOrder/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CustomJewelleryDesignForm
 
 def create_custom_jewellery(request):
@@ -6,15 +6,18 @@ def create_custom_jewellery(request):
         form = CustomJewelleryDesignForm(request.POST, request.FILES)
         if form.is_valid():
             custom_design = form.save()
+            return redirect('CustomOrder:submitted_success', design_name=custom_design.design_name)
     else:
         form = CustomJewelleryDesignForm()
 
     context = {'form': form}
     return render(request, 'CustomOrder/design_jewellery.html', context)
 
+def submitted_success(request):
+    return render(request, 'CustomOrder/submitted_success.html', context)
 
 def view_submitted_forms(request):
-    user_submissions = CustomJewelleryDesign.objects.filter(user=request.user)  # Filter submissions by the current user
+    user_submissions = CustomJewelleryDesign.objects.filter(user=request.user)  
     context = {'user_submissions': user_submissions}
-    return render(request, 'submitted_forms.html', context)
+    return render(request, 'CustomOrder/submitted_forms.html', context)
 
