47a9812ce85ded2d853cbc83a9085dfa8d0b97b9
Aliona83 <alionajanusko@gmail.com>
2023-09-15 12:12:47 +0000

    Deploy to heroku

--- jewelry_shop/settings.py
+++ jewelry_shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
-ALLOWED_HOSTS = ['8000-aliona83-projectj-m4uj85qafch.ws-eu104.gitpod.io']
+ALLOWED_HOSTS = ['jewelryshop-4c4e9a0b748f.herokuapp.com', 'localhost']
 
 
 # Application definition
