d6bcc9e6fbb4042f20a6f3aec50e7112a0dac38b
Aliona83 <alionajanusko@gmail.com>
2023-09-20 15:01:01 +0000

    Change background  color for all buttons

--- jewelry_shop/settings.py
+++ jewelry_shop/settings.py
@@ -27,8 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['jewelryshop-4c4e9a0b748f.herokuapp.com', 'localhost']
+DEBUG = True
+ALLOWED_HOSTS = ['8000-aliona83-projectj-m4uj85qafch.ws-eu104.gitpod.io']
 
 
 # Application definition
