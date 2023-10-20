defd38b96a9017bb1b70281162a820a9722791b5
Aliona83 <alionajanusko@gmail.com>
2023-10-20 08:35:23 +0000

    Make migration

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'jewellery', 'rate', 'comment', 'user', 'created_at', ]
+    readonly_fields = ['user', 'created_at', 'rate', ]
+
+    class Meta:
+        verbose_name_plural = 'Review'
+
--- review/models.py
+++ review/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from jewelleries.models import Jewellery
 
-# Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    jewellery = models.ForeignKey(Jewellery, on_delete=models.CASCADE)
+    comment = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Review by {self.user.username} for {self.jewellery.name}"
+
