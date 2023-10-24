1fb79b13ec545419e0b888d64a6d9efca08a6286
Aliona83 <alionajanusko@gmail.com>
2023-10-24 13:30:58 +0000

    Change host url

--- jewelry_shop/settings.py
+++ jewelry_shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = ['8000-aliona83-projectj-xabbiuzuznv.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['jewelryshop-4c4e9a0b748f.herokuapp.com']
 
 
 # Application definition
